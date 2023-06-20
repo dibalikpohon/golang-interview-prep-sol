e069ed5116c5dc77edf83a1b45aa25bdc9136fd6
dibalikpohon <dibalikpohon@gmail.com>
2023-06-20 08:33:02 +0700

    add some notes to env.sh

--- env.sh
+++ env.sh
@@ -3,4 +3,6 @@
 # Should not have this file pushed to git but
 # for demonstration purpose. This might be not 
 # the best way to store database credentials...
+# It might be better to set permission using chmod 700
+# on this file, i guess?
 alias gorun='GOINTVPR_DB_USER="admin" GOINTVPR_DB_PASSWORD="admin" go run cmd/main.go'
